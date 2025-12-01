f1d54c7ec1263cf4d2a916578d18780afb4206be
DocWilco <github@drwil.co>
2025-12-01 05:44:10 +0100

    Fully use esp_ableton_link as a remote component
    Local bindings file includes header from remote component, this way we
    don't have to update for any changes in Link
    
    Also, apparently `version` is needed on remote components, for
    esp-idf-sys to pick it up correctly. Without it, it silently fails to
    parse the component, and the bindings are not generated.

---
+++ src/bindings.h
@@ -0,0 +1,7 @@
+/* Bindings header for esp-idf-ableton-link
+ *
+ * This file includes the abl_link.h header from the esp_ableton_link component
+ * which is fetched via the ESP-IDF component manager.
+ */
+
+#include "abl_link.h"
